7e391ffb86506fa8c91d6adc7f538deb38f5c6ee
agustin <agustin.moranr@gmail.com>
2021-02-17 14:46:26 -0600

    [ADD] layout inicil del home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 export default function Home() {
   return (
@@ -9,10 +10,25 @@ export default function Home() {
         <title>Twitter Scraper</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Button variant="contained" color="primary">
-        Hola mundo
-      </Button>
+      <Typography align="center" variant="h3" component="h1">
+        Twitter Scraper
+      </Typography>
+      <section className={styles.selectTopic}>
+        <Typography variant="h4" component="h2">
+          {" "}
+          Selecciona Un Tema
+        </Typography>
+        <Button variant="contained" color="primary">
+          #GOVTECH
+        </Button>
+        <Button variant="contained" color="primary">
+          #TECH
+        </Button>
+        <Button variant="contained" color="primary">
+          #FACEBOOK
+        </Button>
+      </section>
+      <section className={styles.tweets}></section>
     </div>
   );
 }
